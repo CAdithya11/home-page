6f5fb20ffd8152ee62eae5feafd25762bb35345a
Anjali Sewmini <anjalisewmini5@gmail.com>
2024-06-04 16:17:30 +0530

    changed Color

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("hello world"); //it show hello world
+
+/*we can write multi line using that
+this is my 2nd line
+this is my 3rd line */
+
+var x = 3;
+console.log(x);
+
+let a = x + 4;  //let == var
+console.log(a);
+
+++a;
+console.log(a);
+
+a++;
+console.log(a);
+
+a--;
+console.log(a);
+
+--a;
+console.log(a);
+
+const b = 365; //days of year(it didn't change)in that time we use 'const'
+console.log(b);
